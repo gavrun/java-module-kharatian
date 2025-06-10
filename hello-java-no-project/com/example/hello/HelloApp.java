cc97162bcf5cf75488f687254c50196b3e5ca49c
gavrun <38539655+gavrun@users.noreply.github.com>
2025-06-10 15:20:25 +0100

    Added project no template

---
+++ hello-java-no-project/com/example/hello/HelloApp.java
@@ -0,0 +1,15 @@
+package com.example.hello;
+
+public class HelloApp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+// javac .\hello-java-no-project\com\example\hello\HelloApp.java
+
+// java .\hello-java-no-project\com\example\hello\HelloApp
+
+// cd .\java-module-kharatian\hello-java-no-project
+// java -cp . com.example.hello.HelloApp
+
